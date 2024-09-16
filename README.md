d6e924194ffdb72111557b1d04632d2520730b0e
PaulinaPyzel <77441300+PaulinaLP@users.noreply.github.com>
2024-09-16 08:53:21 +0000

    modified:   README.md 	modified:   photo.py

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # photo-manipulation
+
+en terminal: 
+sudo apt-get update
+sudo apt-get install libgl1-mesa-glx
+
+en python:
+pip install opencv-python
+pip install pyheif
+
+crear directorios:
+input
+output
+
+En input meter fotos a modificar
--- photo.py
+++ photo.py
@@ -2,19 +2,22 @@ from image_utils import convert_heic_to_jpeg, resize_image, change_background_to
 import os 
 
 CONVERT=1
-RESIZE=0
+RESIZE=1
 CHANGE=0
 # Path
 path = 'input'
 
 # Loop through the folder
 for filename in os.listdir(path):
+    path = 'input'
     file_path = os.path.join(path, filename)  
-    file_name = file_path[:-5]
+    print(file_path)
+    file_name = filename[:-5]
+    print(file_name)
 
     # Convert HEIC to JPEG
     if CONVERT ==1:
-        jpeg_path = 'output/converted_image'+file_name+'.jpg'
+        jpeg_path = 'output/converted_image_'+file_name+'.jpg'
         convert_heic_to_jpeg(file_path, jpeg_path)
         print(f"Converted HEIC to JPEG")
         path = jpeg_path
