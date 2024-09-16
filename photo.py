5df3e144d3c9f1e686387ce0d912f5e015aaa10f
PaulinaPyzel <77441300+PaulinaLP@users.noreply.github.com>
2024-09-16 08:37:24 +0000

    modified:   photo.py

--- photo.py
+++ photo.py
@@ -1,30 +1,36 @@
 from image_utils import convert_heic_to_jpeg, resize_image, change_background_to_white
+import os 
 
 CONVERT=1
 RESIZE=0
 CHANGE=0
-# Paths
-path = 'input/photo1.JPG'
-jpeg_path = 'output/converted_image.jpg'
-resized_path = 'output/resized_image.jpg'
-background_path = 'output/final_carnet_image.jpg'
+# Path
+path = 'input'
 
-# Convert HEIC to JPEG
-if CONVERT ==1:
-    convert_heic_to_jpeg(path, jpeg_path)
-    print(f"Converted HEIC to JPEG")
-    path = jpeg_path
+# Loop through the folder
+for filename in os.listdir(path):
+    file_path = os.path.join(path, filename)  
+    file_name = file_path[:-5]
 
-if RESIZE  ==1:
-    resize_image(path, resized_path, crop_direction='top')
-    print(f"Resized image to tamaño carnet")
-    path = resized_path
+    # Convert HEIC to JPEG
+    if CONVERT ==1:
+        jpeg_path = 'output/converted_image'+file_name+'.jpg'
+        convert_heic_to_jpeg(file_path, jpeg_path)
+        print(f"Converted HEIC to JPEG")
+        path = jpeg_path
 
-if CHANGE ==1:
-    change_background_to_white(path, background_path)
-    print(f"Changed background to white") 
-    path =  background_path     
-   
+    if RESIZE  ==1:
+        resized_path = 'output/resized_image'+file_name+'.jpg'
+        resize_image(path, resized_path, crop_direction='top')
+        print(f"Resized image to tamaño carnet")
+        path = resized_path
+
+    if CHANGE ==1:
+        background_path= 'output/nobackground_image'+file_name+'.jpg'
+        change_background_to_white(path, background_path)
+        print(f"Changed background to white") 
+        path =  background_path     
+    
 
 
 
